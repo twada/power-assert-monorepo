74a62723823d7a2eb49dde8f235092297e18855e
Takuto Wada <takuto.wada@gmail.com>
2023-11-14 05:44:37 +0900

    test: fixture for nested blocks

--- packages/swc-plugin-power-assert/tests/fixtures/blocks/expected.mjs
+++ packages/swc-plugin-power-assert/tests/fixtures/blocks/expected.mjs
@@ -1,4 +1,4 @@
-import { it } from "node:test";
+import { describe, it } from "node:test";
 import assert from "node:assert";
 import { _power_ } from "@power-assert/runtime";
 it("test1", ()=>{
@@ -7,10 +7,21 @@ it("test1", ()=>{
     const falsy = 0;
     _pasrt1.run(_parg1.rec(falsy, 7));
 });
-it("test2", ()=>{
-    const _pasrt2 = _power_(assert, null, "assert(truthy === falsy)");
-    const _parg2 = _pasrt2.recorder(0);
-    const truthy = 0;
-    const falsy = 0;
-    _pasrt2.run(_parg2.rec(_parg2.tap(truthy, 7) === _parg2.tap(falsy, 18), 7));
+describe("description", ()=>{
+    it("test2", ()=>{
+        const _pasrt2 = _power_(assert, null, "assert(truthy === falsy)");
+        const _parg2 = _pasrt2.recorder(0);
+        const truthy = 1;
+        const falsy = 0;
+        _pasrt2.run(_parg2.rec(_parg2.tap(truthy, 7) === _parg2.tap(falsy, 18), 7));
+    });
+    describe("nested", ()=>{
+        it("test3", ()=>{
+            const _pasrt3 = _power_(assert, null, "assert(truthy !== another)");
+            const _parg3 = _pasrt3.recorder(0);
+            const truthy = 0;
+            const another = 0;
+            _pasrt3.run(_parg3.rec(_parg3.tap(truthy, 7) !== _parg3.tap(another, 18), 7));
+        });
+    });
 });
--- packages/swc-plugin-power-assert/tests/fixtures/blocks/input.mjs
+++ packages/swc-plugin-power-assert/tests/fixtures/blocks/input.mjs
@@ -1,11 +1,20 @@
-import { it } from "node:test";
+import { describe, it } from "node:test";
 import assert from "node:assert";
 it("test1", () => {
     const falsy = 0;
     assert(falsy);
 });
-it("test2", () => {
-    const truthy = 0;
-    const falsy = 0;
-    assert(truthy === falsy);
+describe("description", () => {
+    it("test2", () => {
+        const truthy = 1;
+        const falsy = 0;
+        assert(truthy === falsy);
+    });
+    describe("nested", () => {
+        it("test3", () => {
+            const truthy = 0;
+            const another = 0;
+            assert(truthy !== another);
+        });
+    });
 });
