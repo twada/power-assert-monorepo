f3c5ed8b47d7fe593203fdc43840ca47cb3c2e5d
Takuto Wada <takuto.wada@gmail.com>
2023-11-23 21:57:46 +0900

    test(swc-plugin-power-assert): add AwaitExpression fixture

--- packages/transpiler-core/fixtures/AwaitExpression/fixture.mjs
+++ packages/transpiler-core/fixtures/AwaitExpression/fixture.mjs
@@ -1,9 +1,5 @@
 import assert from 'node:assert/strict';
 
 async function myAsync(a){
-  assert((await (a)) === 3);
+  assert(await a === 3);
 }
-
-// function notAsync(a){
-//   assert((await (a)) === 3);
-// }
