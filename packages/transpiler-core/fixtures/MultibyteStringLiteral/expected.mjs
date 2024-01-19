3c457f563df974e7f6e9172e0a6beb3d3908bdce
Takuto Wada <takuto.wada@gmail.com>
2024-01-20 03:20:19 +0900

    test(swc-plugin-power-assert): fixture for multibyte string literal

---
+++ packages/transpiler-core/fixtures/MultibyteStringLiteral/expected.mjs
@@ -0,0 +1,24 @@
+import assert from 'node:assert/strict';
+import {_power_} from "@power-assert/runtime";
+{
+  const _pasrt1 = _power_(assert, null, "assert(fuga !== 'ふが')", {
+    binexp: "!=="
+  });
+  const _parg1 = _pasrt1.recorder(0);
+  _pasrt1.run(_parg1.rec(_parg1.tap(fuga, 7, {
+    hint: "left"
+  }) !== _parg1.tap('ふが', 16, {
+    hint: "right"
+  }), 12));
+}
+{
+  const _pasrt2 = _power_(assert, null, "assert('ほげ' !== 'ふが')", {
+    binexp: "!=="
+  });
+  const _parg2 = _pasrt2.recorder(0);
+  _pasrt2.run(_parg2.rec(_parg2.tap('ほげ', 7, {
+    hint: "left"
+  }) !== _parg2.tap('ふが', 16, {
+    hint: "right"
+  }), 12));
+}
