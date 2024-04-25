6638c2628c3ba156887c74e933c1959ae2e54d3a
Takuto Wada <takuto.wada@gmail.com>
2024-04-26 08:04:27 +0900

    test(transpiler-core): reorganize SpreadElement fixture

--- packages/transpiler-core/fixtures/SpreadElement/expected.mjs
+++ packages/transpiler-core/fixtures/SpreadElement/expected.mjs
@@ -1,13 +1,23 @@
 import assert from 'node:assert/strict';
 import {_power_} from "@power-assert/runtime";
-const _pasrt1 = _power_(assert, null, "assert(hello(...names))");
-const _parg1 = _pasrt1.recorder(0);
-const _pasrt2 = _power_(assert, null, "assert([head, ...tail].length)");
-const _parg2 = _pasrt2.recorder(0);
-const _pasrt3 = _power_(assert, null, "assert(f(head, ...iter(), ...[foo, bar]))");
-const _parg3 = _pasrt3.recorder(0);
-_pasrt1.run(_parg1.rec(hello(..._parg1.tap(names, 16)), 12));
-_pasrt2.run(_parg2.rec(_parg2.tap([_parg2.tap(head, 8), ..._parg2.tap(tail, 17)], 7).length, 23));
-_pasrt3.run(_parg3.rec(f(_parg3.tap(head, 9), ..._parg3.tap(iter(), 22), ..._parg3.tap([_parg3.tap(foo, 30), _parg3.tap(bar, 35)], 29)), 8));
-assert(...iter());
-assert(...[foo, bar]);
+{
+  const _pasrt1 = _power_(assert, null, "assert(hello(...names))");
+  const _parg1 = _pasrt1.recorder(0);
+  _pasrt1.run(_parg1.rec(hello(..._parg1.tap(names, 16)), 12));
+}
+{
+  const _pasrt2 = _power_(assert, null, "assert([head, ...tail].length)");
+  const _parg2 = _pasrt2.recorder(0);
+  _pasrt2.run(_parg2.rec(_parg2.tap([_parg2.tap(head, 8), ..._parg2.tap(tail, 17)], 7).length, 23));
+}
+{
+  const _pasrt3 = _power_(assert, null, "assert(f(head, ...iter(), ...[foo, bar]))");
+  const _parg3 = _pasrt3.recorder(0);
+  _pasrt3.run(_parg3.rec(f(_parg3.tap(head, 9), ..._parg3.tap(iter(), 22), ..._parg3.tap([_parg3.tap(foo, 30), _parg3.tap(bar, 35)], 29)), 8));
+}
+{
+  assert(...iter());
+}
+{
+  assert(...[foo, bar]);
+}
--- packages/transpiler-core/fixtures/SpreadElement/fixture.mjs
+++ packages/transpiler-core/fixtures/SpreadElement/fixture.mjs
@@ -1,11 +1,17 @@
 import assert from 'node:assert/strict';
 
-assert(hello(...names));
-
-assert([head, ...tail].length);
-
-assert(f(head, ...iter(), ...[foo, bar]));
-
-assert(...iter());
-
-assert(...[foo, bar]);
+{
+  assert(hello(...names));
+}
+{
+  assert([head, ...tail].length);
+}
+{
+  assert(f(head, ...iter(), ...[foo, bar]));
+}
+{
+  assert(...iter());
+}
+{
+  assert(...[foo, bar]);
+}
