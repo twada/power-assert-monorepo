d54763db0e81c9a77aec0746cc2376c48ebbd3f4
Takuto Wada <takuto.wada@gmail.com>
2024-06-01 08:09:56 +0900

    chore(node): remove spike example

--- packages/node/examples/spike/test.ts
+++
@@ -1,9 +0,0 @@
-import { test } from 'node:test';
-import { ok as assume } from 'node:assert/strict';
-
-
-test('spike', () => {
-  const falsy: string = '偽';
-  // const truthy: number = falsy + 1;
-  assume('𠮷野家a\u0300で𩸽' === falsy);
-});
