d6225aa90f6eb72a53b06b8163e34174deabfb01
Takuto Wada <takuto.wada@gmail.com>
2024-06-23 07:31:43 +0900

    test(transpiler-core): add more cases to CallExpression fixture

--- packages/transpiler-core/fixtures/CallExpression/fixture.mjs
+++ packages/transpiler-core/fixtures/CallExpression/fixture.mjs
@@ -6,17 +6,40 @@ describe('description', () => {
     const func = () => false;
     assert(func());
   });
-  it('method', () => {
+
+  it('method callee is non-computed MemberExpression', () => {
     const obj = {
       method: () => false
     };
     assert(obj.method());
   });
-  it('computed method', () => {
+
+  it('method callee is computed MemberExpression', () => {
     const methodName = 'method';
     const obj = {
       method: () => false
     };
     assert(obj[methodName]());
   });
+
+  it('method callee is function', () => {
+    const inner = () => ({
+      exact () { return false; }
+    });
+    assert(inner().exact());
+  });
+
+  it('CallExpression of CallExpression of CallExpression', () => {
+    const outer = () => () => () => false;
+    assert(outer()()());
+  });
+
+  it('method callee is non-computed MemberExpression that returns function then invoke immediately', () => {
+    const obj = {
+      method () { return () => () => false; }
+    };
+    assert(obj.method()()());
+  });
+
+
 });
